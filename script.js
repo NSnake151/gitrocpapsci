07aa47fd10a8c71059e99e7499728d86675edb3e
NSnake151 <chris.wright150@gmail.com>
2024-06-17 18:02:09 +0800

    Update logic of script.js
    Updated displayScore to display score to screen.
    Updated check for userContinue to check for specific characters, fixing
    issue where program wouldn't repeat.
    Updated updatescore to increment and return provided value
    Updated playRound to call updateScore depending on the winner of the last round.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log("Hello");
 
 function displayRPKOptions(){
 
-
+    alert("Lets play RPK. Type in 'rock' 'paper' or 'scizzors'.");
 }
 
-function determineRPKWinner(){
+function determineRPKWinner(userChoice, computerChoice){
 
    
 }
@@ -13,24 +13,100 @@ function determineRPKWinner(){
 
 function getComputerRPKChoice(){
 
+    let randomChoice = Math.random() * 100;
+    let computerChoice;
+
+    if(randomChoice < 33)
+    {
+        computerChoice = "rock";
+    }
+    else if(randomChoice < 66)
+    {
+        computerChoice = "paper"
+    }
+    else{
+        computerChoice = "rock"
+    }
+
+    return computerChoice;
 }
 
 function getUserRPKChoice(){
+
+    let choiceValid = false;
+    let userChoice;
+
+    while(choiceValid == false)
+    {
+        userChoice = prompt();
+
+        if(userChoice == "rock" || userChoice == "paper" || userChoice == "scizzors")
+        {
+            choiceValid = true;
+        }
+        else
+        {
+            alert("Invalid choice, please enter again");
+        }
+
+    }
+
+    return userChoice;
+    
 
 
 }
 
 function playRound(){
 
+    displayRPKOptions();
+    computerChoice = getComputerRPKChoice();
+    userChoice = getUserRPKChoice();
+    winner = determineRPKWinner(userChoice, computerChoice);
 
-}
-
-function updateScore(){
-
-}
-
-function displayResults(){
-
+    if(winner == "user")
+    {
+        userScore = updateScore(userScore);
+    }
+    else{
+        computerScore = updateScore(computerScore);
+    }
     
+    displayResults(userScore, computerScore);
+}
+
+function updateScore(score){
+
+    return ++score;
+}
+
+function displayResults(userScore, computerScore){
+    
+    alert("Your score is: " + userScore + ". The computers score is: " + computerScore);
+
+}
+
+function getUserContinue(){
+
+    return prompt("Enter 'y' to continue, or 'q' to quit.");
+
+
+}
+
+
+
+let userContinue = 'y';
+let computerChoice;
+let userChoice;
+let winner;
+let userScore = 0;
+let computerScore = 0;
+
+while(userContinue == 'y')
+{
+    
+    playRound();
+    userContinue = getUserContinue();
+
 }
 
